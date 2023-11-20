92db95dbf7e08b90207fc237577f87b1c9929bf1
VeiShen <veishen.spb@gmail.com>
2023-11-20 10:58:45 +0300

    init

---
+++ chat-server/src/main/java/AuthenticationProvider.java
@@ -0,0 +1,6 @@
+
+public interface AuthenticationProvider {
+    String getUsernameByLoginAndPassword(String login, String password);
+
+    boolean register(String login, String password, String username);
+}
